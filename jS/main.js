e4d0a64e6396163a5c58181bdab58607489add72
Sebastiancorrea12 <scorreabodritto@gmail.com>
2023-02-08 22:02:48 -0300

    Javascript

--- jS/main.js
+++ jS/main.js
@@ -43,17 +43,17 @@ function marcaProductos(){
             alert("El numero ingresado es incorrecto.")
         }
     }
-    while(producto === "lentes de sol")
+    while(marca < 5 || marca  > 1)
 
     switch(marca){
         case 1:
-            return "Orbital"
+            return "Orbital";
         case 2:
-            return "Mormaii"
+            return "Mormaii";
         case 3:
-            return "Vulk"
+            return "Vulk";
         case 4:
-            return "Rusty"
+            return "Rusty";
     }
 }
 function marcalentesReceta(){
@@ -69,16 +69,16 @@ function marcalentesReceta(){
 
     switch(marca){
         case 1:
-            return "Hannover"
+            return "Hannover";
         case 2:
-            return "Usual"
+            return "Usual";
         case 3:
-            return "Amalfi"
+            return "Amalfi";
         case 4:
-            return "Dandy"
+            return "Dandy";
         case 5:
-            return "Ahren"
-        default: return alert("este numero no esta disponible")// esto solo salta cuando es while solo y no do while-
+            return "Ahren";
+        default: return alert("este numero no esta disponible") // esto solo salta cuando es while solo y no do while-
     }
 }
 
@@ -92,21 +92,26 @@ function lentesdeContacto(){
             alert("El numero ingresado es incorrecto.")
         }
     }
-    while(marca < 0 || marca  > 4)//(cuando la condicion de while no se cumpla o sea falsa, vuelve al do)
+    while(marca < 0 || marca  > 4) //(cuando la condicion de while no se cumpla o sea falsa, vuelve al do)
 
     switch(marca){
         case 1:
-            return "Lunare"
+            return "Lunare";
         case 2:
-            return "Proclear"
+            return "Proclear";
         case 3:
-            return "Coopervision"
+            return "Coopervision";
         case 4:
-            return "Alcari"
+            return "Alcari";
     }
 }
 
-function precio(marca)
+listaProductos()
+marcaProductos()
+marcalentesReceta()
+lentesdeContacto()
+
+function precio(marca){
     if(marca === "Orbital"){
         return 15400;
     }
@@ -118,37 +123,61 @@ function precio(marca)
     }
     else if(marca === "Rusty"){
         return 13500;
-    }
-    else if(marca === "Hannover"){
-        return 16000;
-    }
-    else if(marca === "Usual"){
-        return 7600;
-    }
-    else if(marca === "Ahren"){
-        return 12000;
-    }
-    else if(marca === "Amalfi"){
-        return 12500;
-    }
-    else if(marca === "Dandy"){
-        return 19000;
-    }
-    else if(marca === "Lunare"){
-        return 13000;
-    }
-    else if(marca === "Proclear"){
-        return 20200;
-    }
-    else if(marca === "Alcari"){
-        return 17000;
-    }
-    else if(marca === "Coopervision"){
-        return 11100;
-    }
-listaProductos()
+    }}
+//     else if(marca === "Hannover"){
+//         return 16000;
+//     }
+//     else if(marca === "Usual"){
+//         return 7600;
+//     }
+//     else if(marca === "Ahren"){
+//         return 12000;
+//     }
+//     else if(marca === "Amalfi"){
+//         return 12500;
+//     }
+//     else if(marca === "Dandy"){
+//         return 19000;
+//     }
+//     else if(marca === "Lunare"){
+//         return 13000;
+//     }
+//     else if(marca === "Proclear"){
+//         return 20200;
+//     }
+//     else if(marca === "Alcari"){
+//         return 17000;
+//     }
+//     else if(marca === "Coopervision"){
+//         return 11100;
+//     }
+// }
+    
+// precio()
 
-marcaProductos()
-marcalentesReceta()
-lentesdeContacto()
-precio()
+
+   function pagar(precio,marca)
+     {alert("estas a punto de comprar" + marca +"\nPor un precio de: $" + precio)}
+     let pago = prompt("Como deseas pagar?\n1)Efectivo\n2)Transferencia\n3)Débito")
+     switch(pago){
+         case "1":
+       let efectivo = prompt("Seleccionaste Efectivo, ingresa el monto con el que vas a abonar el producto")
+       if (efectivo > precio) {
+         alert("Gracias por tu compra, tu vuelto es: $" + (efectivo - precio));
+         }else if (efectivo == precio) {
+             alert("Gracias por tu compra, pagaste con la cantidad justa de dinero");
+         }else{
+             alert("El monto con el que deseas pagar no es suficiente para realizar la compra.");
+         }
+         break;
+     case "2":
+       alert("Envia el pago al alias Optica.Ruedas y envianos el comprobante de pago a travez de WhatsApp\nLuego de esto podes pasar a retirar tu producto por el local cuando lo desees.")
+       break;
+     case "3":
+       let numCard = prompt("Ingresa el número de tu tarjeta de débito, no compartiremos estos datos con nadie.")
+       alert("Tu tarjeta nº "+ numCard +"fue validada con exito, gracias por la compra y disfruta de tu producto.")
+       break;
+ } 
+
+pagar()
+// efectivo()
